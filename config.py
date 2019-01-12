4889d66e0d760ce3ef8c031c57416c5fd698d98f
Admin <krototip@gmail.com>
2019-01-12 12:37:10 +0300

    auto commit

--- config.py
+++ config.py
@@ -2,8 +2,12 @@
 
 # Бот с именем PomothBot
 
-token = '531376255:AAGkYzkzW7qRzY_vzvhKRCjzB3sBlMSGH6I'
+#token = '531376255:AAGkYzkzW7qRzY_vzvhKRCjzB3sBlMSGH6I'
 
 # Бот с именeм @test1819_bot
 
 # token = '775475818:AAF4aqNyw4r_izXmu_H9_bcg2fb-dE7JO9U'
+
+# Бот с именем @ishod_test_bot.
+
+ token = '676055504:AAEO_XW1g9J5bbNGhnN35xrOdTYB6NCZoOI'
